890b6042c1c1d9108533a1ff53bcd25efc8f440e
Jawad_Rustom <jawad.ru1@gmail.com>
2023-06-09 05:30:46 +0300

    Add Action showPatientProfile to PatientController with api docs Just Have Problem With EditPatientProfile

--- routes/Api/PatientDashbord.php
+++ routes/Api/PatientDashbord.php
@@ -6,4 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/Patient/Dashbord', [PatientController::class, 'Dashbord']);
 Route::get('/Patient/PatientTest', [PatientController::class, 'PatientTest']);
 Route::get('/Patient/PatientAppointment', [PatientController::class, 'PatientAppointment']);
+Route::get('/Patient/EditPatientProfile', [PatientController::class, 'EditPatientProfile']);
+Route::get('/Patient/showPatientProfile', [PatientController::class, 'showPatientProfile']);
 
