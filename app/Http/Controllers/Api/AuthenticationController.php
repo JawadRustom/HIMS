c95cead7d4bbe9f72dfee451335e6cef7121e485
JawadRustom <jawad.ru1@gmail.com>
2023-07-10 02:35:47 +0300

    Try Fix logout 2

--- app/Http/Controllers/Api/AuthenticationController.php
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -163,11 +163,11 @@ class AuthenticationController extends Controller
    */
     public function logout(Request $request)
     {
-    Auth::guard('web')->logout();
+    // Auth::guard('web')->logout();
 
-    $request->session()->invalidate();
+    // $request->session()->invalidate();
 
-    $request->session()->regenerateToken();
+    // $request->session()->regenerateToken();
 
     auth()->user()->currentAccessToken()->delete();
 
